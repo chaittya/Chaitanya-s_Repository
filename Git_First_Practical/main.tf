9f04facda38e11ebf8e5365ee2f0e69addec5a08
chaittya <chaittya@gmail.com>
2025-04-16 21:06:43 +0530

    MRT and CRT added to the main.tf

--- Git_First_Practical/main.tf
+++ Git_First_Practical/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "chaittyavpc" {
+resource "aws_vpc" "Chaitus_VPC" {
   cidr_block = "12.0.0.0/16"
 } 
 # public SUBNET  creation
@@ -11,4 +11,21 @@ resource "aws_subnet" "PUBLIC" {
 resource "aws_subnet" "PRIVATE" {
   vpc_id     = aws_vpc.Chaitus_VPC.id
   cidr_block = "12.0.1.0/24"
+}
+#  ROUTE TABLE creation for public SUBNET  ,routes all traffic to IGW
+resource "aws_route_table" "MYRT" {
+  vpc_id = aws_vpc.Chaitus_VPC.id
+  route {
+    cidr_block = "0.0.0.0/0" # Route all external traffic to the internet
+    gateway_id = aws_internet_gateway.MYIGW.id
+  }
+}
+
+# Create a Route Table for Private Subnet (routes traffic through NAT Gateway)
+resource "aws_route_table" "CRT" {
+  vpc_id = aws_vpc.Chaitus_VPC.id
+  route {
+    cidr_block = "0.0.0.0/0" # Route all external traffic through the NAT Gateway
+    gateway_id = aws_nat_gateway.mynat.id
+  }
 }
